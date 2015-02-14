791a032bb283c084d7233f4c340cd3b762b41cb2
yamagh <hiroyuki.yamagami@gmail.com>
2015-02-15 00:41:12 +0900

    Fix for bootswatch-rails

--- rails-template.rb
+++ rails-template.rb
@@ -39,15 +39,51 @@ generate 'rspec:install'
 run 'rm spec/rails_helper.rb'
 copy_file 'templates/rails_helper.rb', 'spec/rails_helper.rb'
 
+
 # Spring
 run 'spring binstub --all'
+
 
 # Guard
 run 'guard init'
 run 'sed -i "" "s;bundle exec rspec;bin/rspec;g" Guardfile'
 
+
+# BootSwatch
+theme = 'simplex'
+generate "bootswatch:install #{theme} --force"
+generate "bootswatch:import #{theme} --force"
+generate "bootswatch:layout #{theme} --force"
+
+css =<<EOF
+/*
+ *= require #{theme}/loader
+ *= require #{theme}/bootswatch
+ */
+EOF
+run "echo '#{css}' >> app/assets/stylesheets/application.css"
+
+js =<<EOF
+//= require #{theme}/loader
+//= require #{theme}/bootswatch
+EOF
+run "echo '#{js}' >> app/assets/javascripts/application.js"
+
+run "echo 'Rails.application.config.assets.precompile += %w( #{theme}.css )' >> config/initializers/assets.rb"
+run "echo 'Rails.application.config.assets.precompile += %w( #{theme}.js )'  >> config/initializers/assets.rb"
+
+run 'rm app/views/layouts/application.html.erb'
+run 'mv app/views/layouts/simplex.html.erb app/views/layouts/application.html.erb'
+
 # git
 git :init
 git add: "."
 git commit: %Q{ -m 'Initial commit' }
+
+puts '========================================'
+puts 'Add variable like below'
+puts 'This fix need until it will be fixed.'
+puts ''
+puts "echo '@zindex-modal-background: 0;' >> app/assets/stylesheets/#{theme}/variables.less"
+puts '========================================'
 
